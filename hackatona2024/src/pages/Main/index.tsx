1ae2201ff52b4ab44de46f94184868b5e603b2a3
Marcello Marcon <marcellomarcon75@gmail.com>
2024-08-31 01:01:47 -0300

    mapa adicionado

--- hackatona2024/src/pages/Main/index.tsx
+++ hackatona2024/src/pages/Main/index.tsx
@@ -10,6 +10,7 @@ export default function Main() {
       />
       <h1>Fala Samurai!</h1>
       <p>Esse é o template typescript básico da Dev Samurai para React.</p>
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1kQ7ejTduJkj4iexvxij9JRza_6lt4eI&ehbc=2E312F" width="640" height="480"></iframe>
     </Container>
   )
 }
